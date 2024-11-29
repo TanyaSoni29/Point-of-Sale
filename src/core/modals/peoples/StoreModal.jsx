1171560192bb915e2243595804b8b1dd72e3b5c3
TanyaSoni2912 <tanu139sona@gmail.com>
2024-11-29 17:35:15 +0530

    add modal working field increase

--- src/core/modals/peoples/StoreModal.jsx
+++ src/core/modals/peoples/StoreModal.jsx
@@ -1,19 +1,23 @@
 /** @format */
 
 import React, { useEffect } from 'react';
-import Select from 'react-select';
+// import Select from 'react-select';
 // import ImageWithBasePath from '../../img/imagewithbasebath';
 // import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from 'react-hook-form';
-import { updateLocation } from '../../../service/operations/locationApi';
-import { updateLocations } from '../../../Slices/locationSlice';
+import {
+	createLocation,
+	updateLocation,
+} from '../../../service/operations/locationApi';
+import { addLocation, updateLocations } from '../../../Slices/locationSlice';
+import { Switch } from 'antd';
 
 const StoreModal = () => {
 	const { location } = useSelector((state) => state.location);
 	const { token } = useSelector((state) => state.auth);
 	const dispatch = useDispatch();
-	console.log('store location---', location);
+	// console.log('store location---', location);
 	const {
 		register: editRegister,
 		handleSubmit: editHandleSubmit,
@@ -23,6 +27,51 @@ const StoreModal = () => {
 			isSubmitSuccessful: isEditSubmitSuccessful,
 		},
 	} = useForm();
+
+	const {
+		register: addRegister,
+		handleSubmit: addHandleSubmit,
+		reset: addReset,
+		formState: { errors: addErrors, isSubmitSuccessful: isAddSubmitSuccessful },
+		getValues,
+		setValue,
+	} = useForm();
+
+	const onAddSubmit = async (data) => {
+		try {
+			const newData = {
+				code: data?.code,
+				companyNumber: data?.companyNumber || '',
+				name: data?.name || '',
+				postcode: data?.postcode || '',
+				generalEmailAddress: data?.generalEmailAddress || '',
+				mainTelephone: data?.mainTelephone || '',
+				address1: data?.address1 || '',
+				address2: data?.address2 || '',
+				address3: data?.address3 || '',
+				address4: data?.address4 || '',
+				storeWebsiteURL: data.storeWebsiteURL || '',
+				adminName: data.adminName || '',
+				adminEmail: data.adminEmail || '',
+				accountName: data.accountName || '',
+				accountEmail: data.accountEmail || '',
+				keyLocation: data.keyLocation || false,
+				isDeleted: false,
+				id: 0,
+				// dateCreated: '2024-11-29T08:08:40.758',
+				// dateUpdated: '2024-11-29T08:08:40.758',
+				// isValid: true,
+			};
+			const response = await createLocation(token, newData);
+			console.log(response);
+			if (response?.success) {
+				dispatch(addLocation(newData));
+				editReset();
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
 	const onEditSubmit = async (data) => {
 		try {
@@ -48,20 +97,38 @@ const StoreModal = () => {
 	};
 
 	useEffect(() => {
+		if (isAddSubmitSuccessful) {
+			addReset({
+				name: '',
+				adminEmail: '',
+				mainTelephone: '',
+				address1: '',
+				address2: '',
+				address3: '',
+				address4: '',
+			});
+		}
+	}, [addReset, isAddSubmitSuccessful]);
+
+	useEffect(() => {
 		if (isEditSubmitSuccessful) {
 			editReset({
 				name: '',
 				adminEmail: '',
 				mainTelephone: '',
+				address1: '',
+				address2: '',
+				address3: '',
+				address4: '',
 			});
 		}
 	}, [editReset, isEditSubmitSuccessful]);
 
-	const countriesOptions = [
-		{ value: 'choose', label: 'Choose' },
-		{ value: 'unitedKingdom', label: 'United Kingdom' },
-		{ value: 'unitedStates', label: 'United States' },
-	];
+	// const countriesOptions = [
+	// 	{ value: 'choose', label: 'Choose' },
+	// 	{ value: 'unitedKingdom', label: 'United Kingdom' },
+	// 	{ value: 'unitedStates', label: 'United States' },
+	// ];
 
 	// const varrelOptions = [{ value: 'varrel', label: 'Varrel' }];
 
@@ -82,7 +149,7 @@ const StoreModal = () => {
 							<div className='content'>
 								<div className='modal-header border-0 custom-modal-header'>
 									<div className='page-title'>
-										<h4>Add Customer</h4>
+										<h4>Add Location</h4>
 									</div>
 									<button
 										type='button'
@@ -94,8 +161,8 @@ const StoreModal = () => {
 									</button>
 								</div>
 								<div className='modal-body custom-modal-body'>
-									<form>
-										<div className='modal-title-head people-cust-avatar'>
+									<form onSubmit={addHandleSubmit(onAddSubmit)}>
+										{/* <div className='modal-title-head people-cust-avatar'>
 											<h6>Avatar</h6>
 										</div>
 										<div className='new-employee-field'>
@@ -118,15 +185,32 @@ const StoreModal = () => {
 													</div>
 												</div>
 											</div>
-										</div>
+										</div> */}
 										<div className='row'>
 											<div className='col-lg-4 pe-0'>
 												<div className='mb-3'>
-													<label className='form-label'>Store Name</label>
+													<label className='form-label'>Location Code</label>
 													<input
 														type='text'
 														className='form-control'
+														{...addRegister('code', { required: true })}
 													/>
+													{addErrors.code && (
+														<p className=''>{addErrors.code.message}</p>
+													)}
+												</div>
+											</div>
+											<div className='col-lg-4 pe-0'>
+												<div className='mb-3'>
+													<label className='form-label'>Location Name</label>
+													<input
+														type='text'
+														className='form-control'
+														{...addRegister('name', { required: true })}
+													/>
+													{addErrors.name && (
+														<p className=''>{addErrors.name.message}</p>
+													)}
 												</div>
 											</div>
 											<div className='col-lg-4 pe-0'>
@@ -135,7 +219,11 @@ const StoreModal = () => {
 													<input
 														type='email'
 														className='form-control'
+														{...addRegister('adminEmail', { required: true })}
 													/>
+													{addErrors.adminEmail && (
+														<p className=''>{addErrors.adminEmail.message}</p>
+													)}
 												</div>
 											</div>
 											<div className='col-lg-4 pe-0'>
@@ -143,22 +231,197 @@ const StoreModal = () => {
 													<label className='mb-2'>Phone</label>
 													<input
 														className='form-control form-control-lg group_formcontrol'
-														id='phone'
-														name='phone'
+														{...addRegister('mainTelephone', {
+															required: true,
+														})}
 														type='text'
 													/>
+													{addErrors.mainTelephone && (
+														<p className=''>
+															{addErrors.mainTelephone.message}
+														</p>
+													)}
+												</div>
+											</div>
+											<div className='col-lg-4 pe-0'>
+												<div className='mb-3'>
+													<label className='form-label'>Company Number</label>
+													<input
+														type='text'
+														className='form-control'
+														{...addRegister('componyNumber')}
+													/>
+													{addErrors.companyNumber && (
+														<p className=''>
+															{addErrors.companyNumber.message}
+														</p>
+													)}
+												</div>
+											</div>
+											<div className='col-lg-4 pe-0'>
+												<div className='mb-3'>
+													<label className='form-label'>
+														General Email Add.
+													</label>
+													<input
+														type='text'
+														className='form-control'
+														{...addRegister('generalEmailAddress')}
+													/>
+													{addErrors.generalEmailAddress && (
+														<p className=''>
+															{addErrors.generalEmailAddress.message}
+														</p>
+													)}
 												</div>
 											</div>
 											<div className='col-lg-12 pe-0'>
 												<div className='mb-3'>
-													<label className='form-label'>Address</label>
+													<label className='form-label'>Address 1</label>
 													<input
 														type='text'
 														className='form-control'
+														{...addRegister('address1', {
+															required: true,
+														})}
 													/>
+													{addErrors.address1 && (
+														<p className=''>{addErrors.address1.message}</p>
+													)}
 												</div>
 											</div>
-											<div className='col-lg-6 pe-0'>
+											<div className='col-lg-12 pe-0'>
+												<div className='mb-3'>
+													<label className='form-label'>Address 2</label>
+													<input
+														type='text'
+														className='form-control'
+														{...addRegister('address2', {
+															required: true,
+														})}
+													/>
+													{addErrors.address2 && (
+														<p className=''>{addErrors.address2.message}</p>
+													)}
+												</div>
+											</div>
+											<div className='col-lg-12 pe-0'>
+												<div className='mb-3'>
+													<label className='form-label'>Address 3</label>
+													<input
+														type='text'
+														className='form-control'
+														{...addRegister('address3')}
+													/>
+													{addErrors.address3 && (
+														<p className=''>{addErrors.address3.message}</p>
+													)}
+												</div>
+											</div>
+											<div className='col-lg-12 pe-0'>
+												<div className='mb-3'>
+													<label className='form-label'>Address 4</label>
+													<input
+														type='text'
+														className='form-control'
+														{...addRegister('address4')}
+													/>
+													{addErrors.address4 && (
+														<p className=''>{addErrors.address4.message}</p>
+													)}
+												</div>
+											</div>
+											<div className='col-lg-4 pe-0'>
+												<div className='mb-3'>
+													<label className='form-label'>Postcode</label>
+													<input
+														type='text'
+														className='form-control'
+														{...addRegister('postcode')}
+													/>
+													{addErrors.postcode && (
+														<p className=''>{addErrors.postcode.message}</p>
+													)}
+												</div>
+											</div>
+
+											<div className='col-lg-4 pe-0'>
+												<div className='mb-3'>
+													<label className='form-label'>
+														Store Website URL
+													</label>
+													<input
+														type='text'
+														className='form-control'
+														{...addRegister('storeWebsiteURL')}
+													/>
+													{addErrors.storeWebsiteURL && (
+														<p className=''>
+															{addErrors.storeWebsiteURL.message}
+														</p>
+													)}
+												</div>
+											</div>
+
+											<div className='col-lg-4 pe-0'>
+												<div className='mb-3'>
+													<label className='form-label'>Admin Name</label>
+													<input
+														type='text'
+														className='form-control'
+														{...addRegister('adminName')}
+													/>
+													{addErrors.adminName && (
+														<p className=''>{addErrors.adminName.message}</p>
+													)}
+												</div>
+											</div>
+
+											<div className='col-lg-4 pe-0'>
+												<div className='mb-3'>
+													<label className='form-label'>Account Name</label>
+													<input
+														type='text'
+														className='form-control'
+														{...addRegister('accountName')}
+													/>
+													{addErrors.accountName && (
+														<p className=''>{addErrors.accountName.message}</p>
+													)}
+												</div>
+											</div>
+
+											<div className='col-lg-4 pe-0'>
+												<div className='mb-3'>
+													<label className='form-label'>Account Email</label>
+													<input
+														type='text'
+														className='form-control'
+														{...addRegister('accountEmail')}
+													/>
+													{addErrors.accountEmail && (
+														<p className=''>{addErrors.accountEmail.message}</p>
+													)}
+												</div>
+											</div>
+
+											<div className='col-lg-1 pe-0'>
+												<div className='mb-3'>
+													<label className='form-label'>Key Location</label>
+													<Switch
+														checked={getValues('keyLocation')} // Get the current value of the switch
+														onChange={(checked) =>
+															setValue('keyLocation', checked)
+														}
+														{...addRegister('keyLocation')}
+													/>
+													{addErrors.keyLocation && (
+														<p className=''>{addErrors.keyLocation.message}</p>
+													)}
+												</div>
+											</div>
+
+											{/* <div className='col-lg-6 pe-0'>
 												<div className='mb-3'>
 													<label className='form-label'>City</label>
 													<input
@@ -185,7 +448,7 @@ const StoreModal = () => {
 													/>
 													<p>Maximum 60 Characters</p>
 												</div>
-											</div>
+											</div> */}
 										</div>
 										<div className='modal-footer-btn'>
 											<button
@@ -272,8 +535,8 @@ const StoreModal = () => {
 														defaultValue={location?.name}
 														{...editRegister('name', { required: true })}
 													/>
-													{editErrors.name && (
-														<p className=''>{editErrors.name.message}</p>
+													{editErrors?.name && (
+														<p className=''>{editErrors?.name.message}</p>
 													)}
 												</div>
 											</div>
@@ -286,8 +549,8 @@ const StoreModal = () => {
 														defaultValue={location?.adminEmail}
 														{...editRegister('adminEmail', { required: true })}
 													/>
-													{editErrors.adminEmail && (
-														<p className=''>{editErrors.adminEmail.message}</p>
+													{editErrors?.adminEmail && (
+														<p className=''>{editErrors?.adminEmail.message}</p>
 													)}
 												</div>
 											</div>
