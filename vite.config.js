e9e5c4c61b693cdd9a278d65e0fce119f4951bdc
TanyaSoni2912 <tanu139sona@gmail.com>
2024-12-02 11:25:37 +0530

    vite configuration setup

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 	plugins: [react()],
 	rules: [],
 	server: {
-		host: '192.168.1.39', // Replace with your desired IP address
-		port: 3000, // Optional: specify the port as well
+		host: '192.168.1.5', // Replace with your desired IP address
+		port: 80, // Optional: specify the port as well
 	},
 });
