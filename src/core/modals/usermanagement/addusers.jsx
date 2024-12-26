467b86840a6d71a3d1f7e13c010936c48b7e53d7
TanyaSoni2912 <tanu139sona@gmail.com>
2024-12-26 16:20:09 +0530

    Create Staffuser now added properly

--- src/core/modals/usermanagement/addusers.jsx
+++ src/core/modals/usermanagement/addusers.jsx
@@ -1,49 +1,115 @@
-import { PlusCircle } from 'feather-icons-react/build/IconComponents'
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import Select from 'react-select'
+/** @format */
+
+// import { PlusCircle } from 'feather-icons-react/build/IconComponents';
+import React, { useEffect, useRef } from 'react';
+import { useForm } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
+// import { Link } from 'react-router-dom';
+import { createStaffUsers } from '../../../service/operations/staffUsersApi';
+import { refreshStaffUsers } from '../../../slices/staffUserSlice';
+import { Switch } from 'antd';
+// import Select from 'react-select';
 
 const AddUsers = () => {
-    const status = [
-        { value: 'Choose', label: 'Choose' },
-        { value: 'Manager', label: 'Manager' },
-        { value: 'Admin', label: 'Admin' },
-    ];
-    const [showPassword, setShowPassword] = useState(false);
+	const dispatch = useDispatch();
+	const { token } = useSelector((state) => state.auth);
+	const {
+		register,
+		handleSubmit,
+		setValue,
+		watch,
+		formState: { errors, isSubmitSuccessful },
+		reset,
+	} = useForm({
+		defaultValues: {
+			code: '',
+			name: '',
+			allowDiscount: false,
+			allowPriceChange: false,
+			pin: '',
+			allowCredit: false,
+			isMechanic: false,
+		},
+	});
 
-    const handleTogglePassword = () => {
-      setShowPassword((prevShowPassword) => !prevShowPassword);
-    };
-    const [showConfirmPassword, setConfirmPassword] = useState(false);
-    const handleToggleConfirmPassword = () => {
-        setConfirmPassword((prevShowPassword) => !prevShowPassword);
-    };
-  
-    return (
-        <div>
-            {/* Add User */}
-            <div className="modal fade" id="add-units">
-                <div className="modal-dialog modal-dialog-centered custom-modal-two">
-                    <div className="modal-content">
-                        <div className="page-wrapper-new p-0">
-                            <div className="content">
-                                <div className="modal-header border-0 custom-modal-header">
-                                    <div className="page-title">
-                                        <h4>Add User</h4>
-                                    </div>
-                                    <button
-                                        type="button"
-                                        className="close"
-                                        data-bs-dismiss="modal"
-                                        aria-label="Close"
-                                    >
-                                        <span aria-hidden="true">×</span>
-                                    </button>
-                                </div>
-                                <div className="modal-body custom-modal-body">
-                                    <form>
-                                        <div className="row">
-                                            <div className="col-lg-12">
+	const modalRef = useRef(null);
+
+	const allowDiscount = watch('allowDiscount');
+	const allowPriceChange = watch('allowPriceChange');
+	const allowCredit = watch('allowCredit');
+	const isMechanic = watch('isMechanic');
+
+	const onSubmit = async (data) => {
+		try {
+			const response = await createStaffUsers(token, data);
+			if (response?.success) {
+				if (modalRef.current) {
+					modalRef.current.click();
+				}
+				dispatch(refreshStaffUsers());
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		if (isSubmitSuccessful) {
+			reset({
+				code: '',
+				name: '',
+				allowDiscount: false,
+				allowPriceChange: false,
+				pin: '',
+				allowCredit: false,
+				isMechanic: false,
+			});
+		}
+	}, [reset, isSubmitSuccessful]);
+
+	// const status = [
+	// 	{ value: 'Choose', label: 'Choose' },
+	// 	{ value: 'Manager', label: 'Manager' },
+	// 	{ value: 'Admin', label: 'Admin' },
+	// ];
+	// const [showPassword, setShowPassword] = useState(false);
+
+	// const handleTogglePassword = () => {
+	// 	setShowPassword((prevShowPassword) => !prevShowPassword);
+	// };
+	// const [showConfirmPassword, setConfirmPassword] = useState(false);
+	// const handleToggleConfirmPassword = () => {
+	// 	setConfirmPassword((prevShowPassword) => !prevShowPassword);
+	// };
+
+	return (
+		<div>
+			{/* Add User */}
+			<div
+				className='modal fade'
+				id='add-units'
+			>
+				<div className='modal-dialog modal-dialog-centered custom-modal-two'>
+					<div className='modal-content'>
+						<div className='page-wrapper-new p-0'>
+							<div className='content'>
+								<div className='modal-header border-0 custom-modal-header'>
+									<div className='page-title'>
+										<h4>Add User</h4>
+									</div>
+									<button
+										type='button'
+										className='close'
+										data-bs-dismiss='modal'
+										aria-label='Close'
+										ref={modalRef}
+									>
+										<span aria-hidden='true'>×</span>
+									</button>
+								</div>
+								<div className='modal-body custom-modal-body'>
+									<form onSubmit={handleSubmit(onSubmit)}>
+										{/* <div className="col-lg-12">
                                                 <div className="new-employee-field">
                                                     <span>Avatar</span>
                                                     <div className="profile-pic-upload mb-2">
@@ -63,101 +129,203 @@ const AddUsers = () => {
                                                         </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                            <div className="col-lg-6">
-                                                <div className="input-blocks">
-                                                    <label>User Name</label>
-                                                    <input type="text" className="form-control" />
-                                                </div>
-                                            </div>
-                                            <div className="col-lg-6">
-                                                <div className="input-blocks">
-                                                    <label>Phone</label>
-                                                    <input type="text" className="form-control" />
-                                                </div>
-                                            </div>
-                                            <div className="col-lg-6">
-                                                <div className="input-blocks">
-                                                    <label>Email</label>
-                                                    <input type="email" className="form-control" />
-                                                </div>
-                                            </div>
-                                            <div className="col-lg-6">
-                                                <div className="input-blocks">
-                                                    <label>Role</label>
+                                            </div> */}
+										<div className='mb-3'>
+											<div className='input-blocks'>
+												<label>Code</label>
+												<input
+													type='text'
+													className='form-control'
+													{...register('code', {
+														required: 'Code is required',
+													})}
+													placeholder='Enter Code'
+												/>
+												{errors?.code && (
+													<p className='text-danger'>{errors?.code?.message}</p>
+												)}
+											</div>
+										</div>
+										<div className='mb-3'>
+											<div className='input-blocks'>
+												<label>Name</label>
+												<input
+													type='text'
+													className='form-control'
+													{...register('name', {
+														required: 'Staff User Name is required',
+													})}
+													placeholder='Enter Staff User Name'
+												/>
+												{errors?.name && (
+													<p className='text-danger'>{errors?.name?.message}</p>
+												)}
+											</div>
+										</div>
+										<div className='mb-3'>
+											<div className='input-blocks'>
+												<label>Pin</label>
+												<input
+													type='text'
+													className='form-control'
+													{...register('pin')}
+													placeholder='Enter pin'
+												/>
+												{errors?.pin && (
+													<p className='text-danger'>{errors?.pin?.message}</p>
+												)}
+											</div>
+										</div>
+										{/* <div className='col-lg-6'>
+											<div className='input-blocks'>
+												<label>Phone</label>
+												<input
+													type='text'
+													className='form-control'
+												/>
+											</div>
+										</div>
+										<div className='col-lg-6'>
+											<div className='input-blocks'>
+												<label>Email</label>
+												<input
+													type='email'
+													className='form-control'
+												/>
+											</div>
+										</div> */}
+										{/* <div className='col-lg-6'>
+												<div className='input-blocks'>
+													<label>Role</label>
 
-                                                    <Select
-                                                        classNamePrefix="react-select"
-                                                        options={status}
-                                                        placeholder="Choose Status"
-                                                    />
-                                                </div>
-                                            </div>
-                                            <div className="col-lg-6">
-                                                <div className="input-blocks">
-                                                    <label>Password</label>
-                                                    <div className="pass-group">
-                                                        <input
-                                                            type={showPassword ? 'text' : 'password'}
-                                                            className="pass-input"
-                                                            placeholder="Enter your password"
-                                                        />
-                                                        <span
-                                                            className={`fas toggle-password ${showPassword ? 'fa-eye' : 'fa-eye-slash'}`}
-                                                            onClick={handleTogglePassword}
-                                                        />
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="col-lg-6">
-                                                <div className="input-blocks">
-                                                    <label>Confirm Passworrd</label>
-                                                    <div className="pass-group">
-                                                        <input
-                                                            type={showConfirmPassword ? 'text' : 'password'}
-                                                            className="pass-input"
-                                                            placeholder="Enter your password"
-                                                        />
-                                                        <span
-                                                            className={`fas toggle-password ${showConfirmPassword ? 'fa-eye' : 'fa-eye-slash'}`}
-                                                            onClick={handleToggleConfirmPassword}
-                                                        />
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="col-lg-12">
-                                                <div className="mb-0 input-blocks">
-                                                    <label className="form-label">Descriptions</label>
-                                                    <textarea
-                                                        className="form-control mb-1"
-                                                        defaultValue={"Type Message"}
-                                                    />
-                                                    <p>Maximum 600 Characters</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="modal-footer-btn">
-                                            <button
-                                                type="button"
-                                                className="btn btn-cancel me-2"
-                                                data-bs-dismiss="modal"
-                                            >
-                                                Cancel
-                                            </button>
-                                            <Link to="#" className="btn btn-submit">
-                                                Submit
-                                            </Link>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {/* /Add User */}
-        </div>
-    )
-}
+													<Select
+														classNamePrefix='react-select'
+														options={status}
+														placeholder='Choose Status'
+													/>
+												</div>
+											</div> */}
+										{/* <div className='col-lg-6'>
+												<div className='input-blocks'>
+													<label>Password</label>
+													<div className='pass-group'>
+														<input
+															type={showPassword ? 'text' : 'password'}
+															className='pass-input'
+															placeholder='Enter your password'
+														/>
+														<span
+															className={`fas toggle-password ${
+																showPassword ? 'fa-eye' : 'fa-eye-slash'
+															}`}
+															onClick={handleTogglePassword}
+														/>
+													</div>
+												</div>
+											</div>
+											<div className='col-lg-6'>
+												<div className='input-blocks'>
+													<label>Confirm Passworrd</label>
+													<div className='pass-group'>
+														<input
+															type={showConfirmPassword ? 'text' : 'password'}
+															className='pass-input'
+															placeholder='Enter your password'
+														/>
+														<span
+															className={`fas toggle-password ${
+																showConfirmPassword ? 'fa-eye' : 'fa-eye-slash'
+															}`}
+															onClick={handleToggleConfirmPassword}
+														/>
+													</div>
+												</div>
+											</div> */}
+										<div className='mb-1'>
+											<label className=''>
+												<Switch
+													checked={isMechanic}
+													onChange={(value) => {
+														setValue('isMechanic', value);
+													}}
+													style={{ marginRight: '6px' }}
+												/>
+												Is Mechanic
+											</label>
+										</div>
+										<div className='mb-1'>
+											<label className=''>
+												<Switch
+													checked={allowCredit}
+													onChange={(value) => {
+														setValue('allowCredit', value);
+													}}
+													style={{ marginRight: '6px' }}
+												/>
+												Allow Credit
+											</label>
+										</div>
+										<div className='mb-1'>
+											<label className=''>
+												<Switch
+													checked={allowDiscount}
+													onChange={(value) => {
+														setValue('allowDiscount', value);
+													}}
+													style={{ marginRight: '6px' }}
+												/>
+												Allow Discount
+											</label>
+										</div>
+										<div className='mb-1'>
+											<label className=''>
+												<Switch
+													checked={allowPriceChange}
+													onChange={(value) => {
+														setValue('allowPriceChange', value);
+													}}
+													style={{ marginRight: '6px' }}
+												/>
+												Allow Price Change
+											</label>
+										</div>
 
-export default AddUsers
+										{/* <div className='col-lg-12'>
+												<div className='mb-0 input-blocks'>
+													<label className='form-label'>Descriptions</label>
+													<textarea
+														className='form-control mb-1'
+														defaultValue={'Type Message'}
+													/>
+													<p>Maximum 600 Characters</p>
+												</div>
+											</div> */}
+
+										<div className='modal-footer-btn'>
+											<button
+												type='button'
+												className='btn btn-cancel me-2'
+												data-bs-dismiss='modal'
+											>
+												Cancel
+											</button>
+											<button
+												type='submit'
+												className='btn btn-submit'
+											>
+												Submit
+											</button>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			{/* /Add User */}
+		</div>
+	);
+};
+
+export default AddUsers;
